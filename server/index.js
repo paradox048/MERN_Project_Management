de225c1ec3788afa659d666adeb5c8b0e6995392
Derek <dduong03@uoguelph.ca>
2024-01-06 18:39:56 -0500

    backend CRUD functionality

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// Desc: Entry point for the server
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+const { graphqlHTTP } = require('express-graphql');
+const port = process.env.PORT || 5000;
+
+// Import schema
+const schema = require('./schema/schema');
+const app = express();
+//connect to database
+connectDB();
+app.use(
+    '/graphql', 
+    graphqlHTTP(
+    {
+        schema,
+        graphiql: process.env.NODE_ENV === 'development'
+    }
+));
+
+// Start server
+app.listen(port, console.log(`Server is running on port ${port}`));
